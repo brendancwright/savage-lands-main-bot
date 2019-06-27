5aaceb981231858783b4de8ca55ca475262035ce
unknown <brendancwright@gmail.com>
2019-06-27 11:24:27 -0400

    Initial commmit

---
+++ app.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const request = require('request');
+
+const apiUrl = "https://api.battlemetrics.com/servers/3801726";
+
+const updateInterval = (1000 * 60) * 3;
+
+const client = new Discord.Client();
+
+function updateActivity() {
+    // require("tls").DEFAULT_ECDH_CURVE = "auto"
+    request({ url: apiUrl, headers: { json: true, Referer: 'discord-rustserverstatus' }, timeout: 10000 }, function (err, res, body) {
+        if (!err) {
+            const jsonData = JSON.parse(body);
+            const server = jsonData.data.attributes;
+            const is_online = server.status;
+            if (is_online == "online") {
+                const players = server.players;
+                const maxplayers = server.maxPlayers;
+                return client.user.setActivity(`${players}/${maxplayers}`);
+            } else {
+                return client.user.setActivity("Offline");
+            }
+        }
+    });
+}
+
+client.on("ready", () => {
+    updateActivity();
+    setInterval(function () {
+        updateActivity();
+    }, updateInterval);
+});
+
+client.login(process.env.TOKEN);
